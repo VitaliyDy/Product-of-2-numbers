8bebce61346fc2cf7abce4d8ced8617a50ee077c
Vitaliy Dybov <VitaliyDybov@gmail.com>
2017-09-18 23:19:30 +0300

    Implement function for getting array of nums from string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    //get first (string)number as array of numbers
+
+    // let firstNumAsArray = first.slice().split('').map(Number);
+    // let tempFirstPartOne;
+    // let tempFirstParTwo;
+    // if(firstNumAsArray.length >= 8) {
+    //     tempFirstPartOne = firstNumAsArray.splice(-16);
+    //     tempFirstParTwo.splice(0,-16);
+    // }
+    
+    function getArrayOfNumsfromStr(string) {
+        let arr = string.slice().split('');
+        let tempArrOfNums = [];
+        while(arr.length > 6 ){
+             tempArrOfNums.unshift(parseInt(arr.splice(-6,6).join(""),10));
+        }
+        tempArrOfNums.unshift(parseInt(arr.join(""),10));
+    return tempArrOfNums;
+    }
+
+    return tempFirtsNum;
+   
+     
 }
