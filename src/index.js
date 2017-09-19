d1f88d133a557281bea0029429253eee95a7c46d
Vitaliy Dybov <VitaliyDybov@gmail.com>
2017-09-19 17:45:52 +0300

    final push. maybe need to refractor sumOfSingleArray() func (too large)

--- src/index.js
+++ src/index.js
@@ -1,78 +1,23 @@
 module.exports = function multiply(first, second) {
-    if( isNaN(+first) || isNaN(second)){
-        throw new Error('params is not digits');
-    }
-    const delimeter = 3; //determines the number of digits for dvision of first and second
-    let result;
-    
-    //get first (string)number as array of numbers   
-    // let firstNumAsArray = first.slice().split('').map(Number);
-    // let tempFirstPartOne;
-    // let tempFirstParTwo;
-    // if(firstNumAsArray.length >= 8) {
-    //     tempFirstPartOne = firstNumAsArray.splice(-16);
-    //     tempFirstParTwo.splice(0,-16);
-    // }
-    let firtsArrOfNum = getArrayOfNumsfromStr(first, delimeter), 
-        secondArrOfNum = getArrayOfNumsfromStr(second, delimeter);
+    const delimeter = 7; //determines the number of digits for dvision of first and second
+    let tempResultArray=[],
+        firtsArrOfNum = [],
+        secondArrOfNum =[],
+        arraysCountInDdArr = 0,
+        result = '';
 
-    //create double dimension array of numbers as strings divided by delimeters
-  //  function multiplyArrays (){
-    let ddArrResultOfmultiply = [];
+    firtsArrOfNum = getArrayOfNumsfromStr(first, delimeter), 
+    secondArrOfNum = getArrayOfNumsfromStr(second, delimeter);
+    ddArrayResultOfMultiply = multiplyArrays(firtsArrOfNum,secondArrOfNum);
+    DdArrLen = ddArrayResultOfMultiply.length;   
+   
+    for (let k = 0; k < DdArrLen; k++) {             
+       tempResultArray.push(sumOfSingleArray(ddArrayResultOfMultiply[k]).join(''));
+    }    
 
-    for (let i = 0, firstArrLen = firtsArrOfNum.length-1; i <= firstArrLen; i++) {
-        ddArrResultOfmultiply[i] = [];
-        for (let j = 0, secondArrLen = secondArrOfNum.length-1; j <= secondArrLen; j++) {
-            ddArrResultOfmultiply[i][j] = [];
-            ddArrResultOfmultiply[i][j].push(firtsArrOfNum[firstArrLen-i]*secondArrOfNum[secondArrLen-j]);
-            ddArrResultOfmultiply[i][j] += addZeros(i+j,delimeter);
-        }
-                    
-    }
-    let arrsInDdarrResuylt = ddArrResultOfmultiply.length;
-   //    return 
- //   }
-
-    /**
-     * Sum numbers(as strings) in array, digit by digit
-     * @param {*string[]} array 
-     * @return {*string[]} 
-     */
-    function sumOfSingleArray (array) {
-        const len = array.length;
-        let FirstNumLen =0,
-            SecNumLen = 0,
-            tempArr = [];
-
-        for (let i = 0; i < len; i++){
-            tempArr.push([array[i].split('')]);
-        }
-        for (let j = 0; j< len-1; j++ ){
-            FirstNumLen = tempArr[j].length;
-            SecNumLen =  tempArr[j+1].length;
-        }
-        
-
-
-
-    }
-
-
-
-    /**
-     * return string of required number of zeros
-     * @param {*int} num 
-     * @param {*int} delimeter
-     * @return {*string}  
-     */
-    function addZeros (num,delimeter) {
-        let zeros =''
-        for (var i = 0; i < num*delimeter; i++) {
-            zeros += '0';
-            
-        }
-        return zeros;
-    }
+    result = sumOfSingleArray(tempResultArray).join('')
+  
+    return result;  
 
     /**
      * returns array of numbers   
@@ -90,7 +35,96 @@ module.exports = function multiply(first, second) {
     return tempArrOfNums;
     }
 
-    return ddArrResultOfmultiply;
-   
-     
+    /**
+     *  create double dimension array of numbers counted by delimeter
+     * @param {*int[]} firtsArr 
+     * @param {*int[]} secondArr 
+     * @return {*string[]} 
+    */
+    function multiplyArrays (firtsArr,secondArr){
+        
+        let resultDdArr = [],
+            firstArrLen = firtsArr.length-1,
+            secondArrLen = secondArr.length-1;
+
+        for (let i = 0 ; i <= firstArrLen; i++) {
+            resultDdArr[i] = [];
+            for (let j = 0 ; j <= secondArrLen; j++) {
+                resultDdArr[i][j] = [];
+                resultDdArr[i][j].push(firtsArr[firstArrLen-i]*secondArr[secondArrLen-j]);
+                resultDdArr[i][j] += addZeros((i+j)*delimeter);
+            }                        
+        }
+        return resultDdArr;
+    }
+
+    /**
+     * Sum numbers(as strings) off array with initArray 
+     * digit by digit, add zero at the beginning of arrays.
+     * @param {*string[]} array 
+     * @param {*string[]} initArray 
+     * @return {*string[]} 
+     */
+    function sumOfSingleArray (array, initArray = []) {      
+        const len = array.length;
+        let firstNumLen = 0,
+            secNumLen = 0,
+            subs = 0,            
+            tempArr = [],
+            firstNum = [],
+            secondNum = initArray;
+
+         if(!array.length){
+             return initArray;
+         }
+
+        for (let i = 0; i < len; i++){
+            tempArr.push(array[i].split(''));           
+        }
+        for (let j = 0; j <  len/*len-1*/; j++ ){
+            firstNum = tempArr[j].slice();      
+            firstNumLen = tempArr[j].length;          
+            secondNumLen = secondNum.length;
+            
+            if (firstNumLen !== secondNumLen) {
+                if (firstNumLen > secondNumLen) {
+                    subs = firstNumLen-secondNumLen+1;
+                    secondNum = addZeros(subs).split('').concat(secondNum);
+                    firstNum = ['0'].concat(firstNum);
+                } else {
+                    subs = secondNumLen - firstNumLen;
+                    firstNum = addZeros(subs).split('').concat(firstNum);
+                    secondNum = ['0'].concat(secondNum);
+                }
+            }
+
+            let flagDelimetr=0;
+            let tempKnum = 0;
+            for( let k = firstNum.length-1 ; 0 <= k; k-- ){                
+                tempKnum = parseInt(secondNum[k], 10) + parseInt(firstNum[k], 10) + flagDelimetr;               
+                flagDelimetr = 0;            
+                if (  tempKnum >= 10 ){
+                    flagDelimetr = 1;                    
+                }
+                secondNum[k] = String(tempKnum % 10);
+            }                    
+        }   
+        while(secondNum[0] === '0' ) {
+            secondNum.shift();
+        }   
+        return  secondNum; 
+    }
+
+    /**
+     * return string of required number of zeros
+     * @param {*int} num
+     * @return {*string}  
+     */
+    function addZeros (num) {
+        let zeros =''
+        for (var i = 0; i < num; i++) {
+            zeros += '0';            
+        }
+        return zeros;
+    }
 }
